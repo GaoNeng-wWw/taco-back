d0be6b08c09e7f334379c1b5a6cd8d356dbdaa6b
GaoNeng-wWw <gaonenggg@outlook.com>
2023-02-09 13:52:53 +0800

    feat: 新增微服务启动

--- apps/micro-service/src/main.ts
+++ apps/micro-service/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { MicroServiceModule } from './micro-service.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MicroServiceModule);
-  await app.listen(3002);
-  console.log('micro service load success');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MicroServiceModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  app.listen();
 }
 bootstrap();
