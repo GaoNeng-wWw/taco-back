645eb758905d45e6d05810000669e045d347f179
GaoNeng-wWw <gaonenggg@outlook.com>
2023-08-02 11:03:14 +0800

    refactor: 删除account模块中不必要的依赖

--- apps/main/src/account/account.module.ts
+++ apps/main/src/account/account.module.ts
@@ -4,17 +4,11 @@ import { AccountController } from './account.controller';
 import { DbModule } from '@app/db';
 import { JwtModule } from '@app/jwt';
 import { ModelDefinitionModule } from '@app/model-definition';
-import { MqModule } from '@app/mq';
+
 import { CacheModule } from '@app/cache';
 
 @Module({
-	imports: [
-		CacheModule,
-		DbModule,
-		JwtModule,
-		ModelDefinitionModule,
-		MqModule,
-	],
+	imports: [CacheModule, DbModule, JwtModule, ModelDefinitionModule],
 	controllers: [AccountController],
 	providers: [AccountService],
 })
